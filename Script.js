28b27bfa1aa7c3909206c3dc54790512627a14ee
TanishiSinhaCoder <SinhaTanishi@outlook.com>
2022-02-10 14:08:35 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,11 @@
+// const deg = 6;
+// const hr = document.querySelector("#hr");
+// const mn = document.querySelector("#mn");
+// const sc = document.querySelector("#sc");
+// setInterval() => { 
+//     let day = new Date();
+//     let hh =day.getHours() * 30;
+//     let hh =day.getMinutes() * deg;
+//     let hh =day.getSeconds() * deg;
+//     hr.getElementsByClassName.transform = `rotateZ(${hh+(mm/12)}deg)`;
+// }
